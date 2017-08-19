52694631a42711fca400ea02f2a43e9c9fdd60c8
amoghkalyanshetti <30956433+amoghkalyanshetti@users.noreply.github.com>
2017-08-19 22:48:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,12 @@ element.innerHTML='new Value';
 
 //move the image
 var img=document.getElementById("imag");
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onClick=function(){
-  img.style.marginLeft='500px';   
+  var interval=setInterval(moveRight,100);//every 100 milliseconds call movRight function
 };
