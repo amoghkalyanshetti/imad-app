0fc558f14ca16d9fc98894b56fe2c0c0718208e5
amoghkalyanshetti <30956433+amoghkalyanshetti@users.noreply.github.com>
2017-08-19 21:31:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,5 +8,5 @@ element.innerHTML='new Value';
 //move the image
 var img=document.getElementById("imag");
 img.onClick=function(){
-  img.style.marginLeft='100px';   
+  img.style.marginLeft='200px';   
 };
