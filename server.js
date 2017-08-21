a3f9b09177e4ef8f1c8d421d896fa2570b482d5d
amoghkalyanshetti <30956433+amoghkalyanshetti@users.noreply.github.com>
2017-08-21 22:58:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,12 @@ function createTemplate(data)
 }
 
 
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -96,11 +102,7 @@ app.get('/:articleName',function (req,res)
    res.send(createTemplate(articles[articleName]));
 });
 
-var counter=0;
-app.get('/counter', function (req, res) {
-    counter=counter+1;
-  res.send(counter.toString())
-});
+
 
 
 
