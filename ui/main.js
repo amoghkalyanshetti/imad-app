c55fb0cf2194160611ea3fa4ced4065c3e3fc3fa
amoghkalyanshetti <30956433+amoghkalyanshetti@users.noreply.github.com>
2017-08-22 18:54:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,35 @@
-console.log('Loaded!');
+// console.log('Loaded!');
 
-//change the text of main-text div
-var element=document.getElementById('main-text'
-);
-element.innerHTML='new Value';
+// //change the text of main-text div
+// var element=document.getElementById('main-text'
+// );
+// element.innerHTML='new Value';
 
-//move the image
-var img=document.getElementById("imag");
-var marginLeft=0;
-function moveRight()
+// //move the image
+// var img=document.getElementById("imag");
+// var marginLeft=0;
+// function moveRight()
+// {
+//     marginLeft=marginLeft+10;
+//     img.style.marginLeft=marginLeft+'px';
+// }
+// img.onClick=function(){
+//   var interval=setInterval(moveRight,100);//every 100 milliseconds call movRight function
+// };
+
+var counter=0;
+button.onclick = function() 
 {
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onClick=function(){
-  var interval=setInterval(moveRight,100);//every 100 milliseconds call movRight function
+    //make request to the counter endpoint
+    
+    //capture the response and store it in a variable
+    
+    //render the variable in  the correct span
+    counter=counter+1;
+    span.innerHTML = counter.toString();
+    
+    
+    
+    
+    
 };
