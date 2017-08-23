89770ee0c75e7d985f7d47f3c70d50dd3bc672c6
amoghkalyanshetti <30956433+amoghkalyanshetti@users.noreply.github.com>
2017-08-23 19:49:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,12 +135,32 @@ app.get('/submit-name',function(req,res)//url=/submit-name?name=xxxx
    
 });
 
-app.get('/:articleName',function (req,res)
+app.get('articles/:articleName',function (req,res)
 {
     //articleName==article-one
     //articles[articleName]=={} content object for article one
-    var articleName=req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+    
+    
+    pool.query("Select * from article where title="+req.params.articleName,function(err,result)
+    {
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            if(result.rows.length === 0)
+            {
+                res.status(400).send('Article Not Found');
+            }
+            else
+            {
+                var articleData=result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+   
 });
 
 
