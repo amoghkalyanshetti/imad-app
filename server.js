a3aa2ce8154981e4ee0d915cc88113e1738f3417
amoghkalyanshetti <30956433+amoghkalyanshetti@users.noreply.github.com>
2017-08-31 20:32:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,6 +25,26 @@ app.get('/hash/:input',function(req,res){
    var hashedString =hash(req.params.input,'this-is-some-random-string');
    res.send(hashedString);
 });
+
+app.post('/create-user',function(req,res){
+   //username password
+   //JSON
+   var username=req.body.username;
+   var password=req.body.password;
+   
+   var salt=crypto.getRandomBytes(128).toString('hex');
+   var dbString=hash(password,salt);
+   pool.query('INSERT INTO "user"(username,password) VALUES($1,$2)',[username,dbString],function(err,result){
+   	 if(err)
+   	 {
+   	 	res.status(500).send(err.toString());
+   	 }
+   	 else
+   	 {
+   	 	res.send('User successfully created'+username);
+   	 }
+   });
+});
 // var articles=
 // {
 //     'article-one':
@@ -186,7 +206,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
+    
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
