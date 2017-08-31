f7a2e4e16185a18defbb92cca73010eb3d801d96
amoghkalyanshetti <30956433+amoghkalyanshetti@users.noreply.github.com>
2017-08-31 20:42:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto=require('crypto');
-
+var bodyParser=require('body-parser')
 var Pool =require('pg').Pool;
 var config=
 {
@@ -15,6 +15,7 @@ var config=
 };
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 function hash(input,salt)
 {
     //How do we create a hash 
@@ -32,7 +33,7 @@ app.post('/create-user',function(req,res){
    var username=req.body.username;
    var password=req.body.password;
    
-   var salt=crypto.getRandomBytes(128).toString('hex');
+   var salt=crypto.randomBytes(128).toString('hex');
    var dbString=hash(password,salt);
    pool.query('INSERT INTO "user"(username,password) VALUES($1,$2)',[username,dbString],function(err,result){
    	 if(err)
