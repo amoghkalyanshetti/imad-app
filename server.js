a45cd33bf200ff093941cfeff744b784289343f8
amoghkalyanshetti <30956433+amoghkalyanshetti@users.noreply.github.com>
2017-08-31 18:52:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var crypto=require('crypto');
+function hash(input,salt)
+{
+    //How do we create a hash 
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+   var hashedString =hash(req.params.input,'this-is-some-random-string');
+   res.send(hashedString);
+});
 var Pool =require('pg').Pool;
 var config=
 {
